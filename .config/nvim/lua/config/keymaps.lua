c9157ba54c2cfd7bc3e6ca7437d89c03b567471f
ifoxser <tifoxser@gmail.com>
2024-11-07 00:03:17 +0800

    Modified LSP keymaps & Keymap J and K for more line jumps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "J", "4j", { desc = "Move down 4 lines" })
+vim.keymap.set("n", "K", "4k", { desc = "Move up 4 lines" })
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,20 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = {
-        virtual_text = false,
-      },
-    },
+    opts = function()
+      ---@class PluginLspOpts
+      local ret = {
+        -- options for vim.diagnostic.config()
+        ---@type vim.diagnostic.Opts
+        diagnostics = {
+          virtual_text = false,
+        },
+      }
+
+      -- LSP keymaps
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "<Leader>ck", vim.lsp.buf.hover, desc = "Hover Documentation" }
+    end,
   },
 }
