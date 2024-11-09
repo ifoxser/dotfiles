b3bdcdb968ca084fc9b5e7eae32ccbea3d2be450
ifoxser <tifoxser@gmail.com>
2024-11-09 12:59:13 +0800

    refine: Modified LSP keymaps

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,19 +1,21 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      ---@class PluginLspOpts
-      local ret = {
-        -- options for vim.diagnostic.config()
-        ---@type vim.diagnostic.Opts
-        diagnostics = {
-          virtual_text = false,
-        },
-      }
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+      },
+    },
+  },
 
-      -- LSP keymaps
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
       keys[#keys + 1] = { "K", false }
+      -- add a keymap
       keys[#keys + 1] = { "<Leader>ck", vim.lsp.buf.hover, desc = "Hover Documentation" }
     end,
   },
