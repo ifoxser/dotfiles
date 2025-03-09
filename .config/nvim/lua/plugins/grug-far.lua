fe40d04c53a16ce8914e8b869d0a3aff0add3961
ifoxser <tifoxser@gmail.com>
2025-03-09 22:50:44 +0800

    feat(nvim): improve grug-far default search behavior
    - Update search to use current filename and directory separately
    - Fix edge cases when buffer has no associated file

--- .config/nvim/lua/plugins/grug-far.lua
+++ .config/nvim/lua/plugins/grug-far.lua
@@ -1,15 +1,23 @@
 return {
   "MagicDuck/grug-far.nvim",
+
   keys = {
     {
       "<leader>sr",
       function()
         local grug = require("grug-far")
-        local ext = vim.bo.buftype == "" and vim.fn.expand("%")
+
+        -- Get current filename without path
+        local filename = vim.fn.expand("%:t")
+
+        -- Get directory path of current file
+        local directory = vim.fn.expand("%:p:h")
+
         grug.open({
           transient = true,
           prefills = {
-            filesFilter = ext or nil,
+            filesFilter = filename ~= "" and filename or nil,
+            paths = directory ~= "" and directory or nil,
           },
         })
       end,
