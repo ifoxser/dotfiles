8275524c72156fd613d2d96a7db738ba9b2f27e8
ifoxser <tifoxser@gmail.com>
2025-03-09 12:49:19 +0800

    feat(nvim): add custom keymaps for fzf-lua
    - Add keybinding <leader><space> for finding files in current directory
    - Add keybinding <leader>/ for live grep in current directory
    - Both commands now respect the current working directory instead of project root

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -16,4 +16,9 @@ return {
 
     return opts
   end,
+
+  keys = {
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+  },
 }
