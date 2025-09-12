c862af84d4d45fdcfb2035a16f982be5d8f94ddd
ifoxser <tifoxser@gmail.com>
2025-09-12 23:55:36 +0800

    feat(nvim): update LazyVim and plugins with theme transparency fix
    - Update LazyVim and all plugins to latest versions
    - Remove transparent_background configuration from catppuccin theme (defaults to false)

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,10 +3,8 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      transparent_background = false,
-    },
   },
+
   {
     "LazyVim/LazyVim",
     opts = {
